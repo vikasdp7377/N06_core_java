529e0720c1ce7183e8143d089920e745e0b91ff6
vikas <vikas@LAPTOP-CDKSISOI>
2023-07-21 12:00:29 +0530

    vbn nv

---
+++ dayone1/src/com/Addition.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,sum;
+		System.out.println("vikas");
+
+	}
+
+}
